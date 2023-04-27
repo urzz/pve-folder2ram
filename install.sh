0a312cef3d606681f63b13b7746b9e1fd1070d76
fooksky <virus.ljd@gmail.com>
2023-04-27 23:59:26 +0800

    fix: add ignore_dirs check

--- install.sh
+++ install.sh
@@ -34,19 +34,40 @@ cat <<EOF >/sbin/trunc_ram_log
 #!/bin/bash
 
 # Truncate all files in a directory (recursively) to the last N lines
-# Usage: truncLog <directory> <number_of_lines>
+# Usage: truncLog <directory> <number_of_lines> [<ignore_dirs>]
 
 processed_files=\$(mktemp)
 trap 'rm -f "\$processed_files"' EXIT
 
 truncLog() {
-    for file in "\$1"/*; do
+    local dir="\$1"
+    local lines="\$2"
+    local ignore_dirs="\$3"
+    for file in "\$dir"/*; do
         if [ -d "\$file" ]; then
-            echo "Processing directory: \$file"
-            truncLog "\$file" "\$2"
+            local skip=false
+            for ignore_dir in \$ignore_dirs; do
+                if [[ "\$file" == "\$ignore_dir"/* ]]; then
+                    echo "Skipping directory: \$file"
+                    skip=true
+                    break
+                fi
+            done
+            if ! "\$skip"; then
+                echo "Processing directory: \$file"
+                truncLog "\$file" "\$lines" "\$ignore_dirs"
+            fi
         elif [ -f "\$file" ]; then
+            local log_files=".journal"
             if grep -q "\$file" "\$processed_files"; then
                 # Skip files that have already been truncated
+                continue
+            fi
+            local file_name=\$(basename "\$file")
+            local file_ext="\${file_name##*.}"
+            if [[ "\$log_files" == *".\$file_ext"* ]]; then
+                # Skip file
+                echo "Skip truncate file: \$file"
                 continue
             fi
             echo "Truncating file: \$file"
@@ -58,30 +79,47 @@ truncLog() {
 }
 
 # Clean up all non-log files in a directory (recursively)
-# Usage: cleanDir <directory>
+# Usage: cleanDir <directory> [<ignore_dirs>]
 
 cleanDir() {
-    local log_files=".log .log. .info .warn .journal"
-    find "\$1" -type f | while read file; do
-        local file_name=\$(basename "\$file")
-        local file_ext="\${file_name##*.}"
-        if [[ "\$log_files" == *".\$file_ext"* ]]; then
-            # Keep log files
-            continue
+    local dir="\$1"
+    local ignore_dirs="\$2"
+
+    for file in "\$dir"/*; do
+        if [ -d "\$file" ]; then
+            local skip=false
+            for ignore_dir in \$ignore_dirs; do
+                if [[ "\$file" == "\$ignore_dir"/* ]]; then
+                    echo "Skipping directory: \$file"
+                    skip=true
+                    break
+                fi
+            done
+            if ! "\$skip"; then
+                echo "Processing directory: \$file"
+                cleanDir "\$file" "\$ignore_dirs"
+            fi
+        elif [ -f "\$file" ]; then
+            local log_files=".log .info .warn .journal"
+            if [[ "\$log_files" == *"\$file"* ]]; then
+                # Keep log files
+                continue
+            fi
+            echo "Removing file: \$file"
+            rm "\$file"
         fi
-        echo "Removing file: \$file"
-        rm "\$file"
     done
 }
 
 # Main script
-if [ "\$#" -ne 2 ]; then
-    echo "Usage: \$0 <directory> <number_of_lines>"
+if [ "\$#" -lt 2 ]; then
+    echo "Usage: \$0 <directory> <number_of_lines> [<ignore_dirs>]"
     exit 1
 fi
 
 dir="\$1"
 lines="\$2"
+ignore_dirs="\${3:-}"
 
 if [ ! -d "\$dir" ]; then
     echo "\$dir is not a directory"
@@ -89,9 +127,9 @@ if [ ! -d "\$dir" ]; then
 fi
 
 echo "Truncating log files in directory: \$dir to \$lines lines"
-truncLog "\$dir" "\$lines"
+truncLog "\$dir" "\$lines" "\$ignore_dirs"
 echo "Cleaning up non-log files in directory: \$dir"
-cleanDir "\$dir"
+cleanDir "\$dir" "\$ignore_dirs"
 EOF
 chmod +x /sbin/trunc_ram_log
 
